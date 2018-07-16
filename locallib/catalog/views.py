60fc099bc65c03617f861458819546d87c12a504
prkksh <38299178+prkksh@users.noreply.github.com>
2018-07-16 12:52:02 +0530

    Book and author pages added

--- locallib/catalog/views.py
+++ locallib/catalog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
 
+from django.views import generic
+
 # Create your views here.
 
 def index(request):
@@ -14,11 +16,27 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
-
+    # Genres and available books
+    num_genres = Genre.objects.all().count()
+    #--num_books_word = Book.objects.filter(name_iexact='Hello')
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_genres': num_genres},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Book
